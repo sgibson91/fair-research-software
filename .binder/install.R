e6be2cfad41519a8d6d0dd85a48dec562e609604
Sarah Gibson <drsarahlgibson@gmail.com>
2023-10-19 12:16:01 +0100

    Add software dependency files to .binder folder so that repo can be launched on mybinder

---
+++ .binder/install.R
@@ -0,0 +1,7 @@
+install.packages("sandpaper")
+install.packages("varnish")
+install.packages("pegboard")
+install.packages("tinkr")
+install.packages(
+    repos = c("https://carpentries.r-universe.dev/", getOption("repos"))
+)
